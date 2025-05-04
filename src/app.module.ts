d57e20a6b2fe613f3d5e9b2c06a84498cc2b4bd9
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-03 23:04:07 -0300

    feat: add CarbonFootprint module and register it in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CarbonFootprintModule } from './carbon-footprint.module'; // ajuste se o caminho for outro
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/carbon_footprint'),
+    CarbonFootprintModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/carbon-footprint.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CarbonFootprintEntrySchema } from '@infra/repositories/schemas/carbon-footprint-entry.schema';
+import { CarbonFootprintEntryMongoRepository } from '@infra/repositories/carbon-footprint-entry-mongo.repository';
+import { CarbonFootprintEntryRepository } from '@domain/repositories/carbon-footprint-entry.repository';
+import { CarbonFootprintController } from '@presentation/controllers/carbon-footprint.controller';
+import { CreateCarbonFootprintEntryUseCase } from '@application/use-cases/create-carbon-footprint-entry/create-carbon-footprint-entry.usecase';
+import { GetCarbonFootprintHistoryUseCase } from '@application/use-cases/get-carbon-footprint-history/get-carbon-footprint-history.usecase';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'CarbonFootprintEntry', schema: CarbonFootprintEntrySchema },
+    ]),
+  ],  
+  controllers: [CarbonFootprintController],
+  providers: [
+    {
+      provide: CarbonFootprintEntryRepository,
+      useClass: CarbonFootprintEntryMongoRepository,
+    },
+    CreateCarbonFootprintEntryUseCase,
+    GetCarbonFootprintHistoryUseCase,
+  ],
+  exports: [CarbonFootprintEntryRepository],
+})
+export class CarbonFootprintModule {}
